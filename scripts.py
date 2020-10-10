1b2adce568684090ebfd277cb993dec47ccae126
minifred <72654963+minifred@users.noreply.github.com>
2020-10-10 14:50:40 +0200

    Update scripts.py
    added binary search

--- scripts.py
+++ scripts.py
@@ -36,4 +36,18 @@ def sum_primes(numberlist):
     return sum
 
 
-print(sum_primes([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+def binary_search_recursive(array, element, start, end):
+    if start > end:
+        return -1
+
+    mid = (start + end) // 2
+    if element == array[mid]:
+        return mid
+
+    if element < array[mid]:
+        return binary_search_recursive(array, element, start, mid-1)
+    else:
+        return binary_search_recursive(array, element, mid+1, end)
+
+
+print(binary_search_recursive([1, 0, 0, 4, 6, 7, 8, 3, 2, 1, 4, 7], 4, 0, 11))
